ca42a592806460a4e7b3e75f2de6a8d3069ddac7
TomKatsa <49811302+TomKatsa@users.noreply.github.com>
2020-04-02 18:19:40 +0300

    Update ConsoleBlur.c

--- ConsoleBlur.c
+++ ConsoleBlur.c
@@ -4,9 +4,11 @@
 
 
 void MakeTransparent(HWND hWnd) {
-    AccentPolicy policy = {ACCENT_ENABLE_BLURBEHIND, 2, 0x80000000,0};
+    // Policy parameters: { AccentState, AccentFlags, GradientColor (0xAARRGGBB), Animationid }
+    // 2 Flag enables the use of gradient color (Making the window half-transparent improves the look on light background)
+    AccentPolicy policy = {ACCENT_ENABLE_BLURBEHIND, 2, 0x80000000,0}; 
     WindowCompositionAttributeData data = {WCA_ACCENT_POLICY, &policy, sizeof(AccentPolicy) };
-    FARPROC FuncPointer = GetProcAddress(GetModuleHandle("user32.dll"), "SetWindowCompositionAttribute");
+    FARPROC FuncPointer = GetProcAddress(GetModuleHandle("user32.dll"), "SetWindowCompositionAttribute"); // Get the undocumented func address
     BOOL (*SetWindowCompositionAttribute)(HWND, WindowCompositionAttributeData*) = (BOOL (*)(HWND, WindowCompositionAttributeData*))FuncPointer;
     SetWindowCompositionAttribute(hWnd, &data);
 }
@@ -15,11 +17,11 @@ void MakeTransparent(HWND hWnd) {
 BOOL CALLBACK EnumWindowsProc(HWND hWnd, LPARAM name) {
     char buff[100];
     GetWindowText(hWnd, buff, 100);
-    if (strcmp((char*)name, buff) == 0) {
+    if (strcmp((char*)name, buff) == 0) { // If window name matches this console name
         MakeTransparent(hWnd);
         return FALSE;
     }
-    return TRUE;
+    return TRUE; // Iterate the window handles
 }
 
 
